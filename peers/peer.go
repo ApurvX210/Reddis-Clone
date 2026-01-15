33e205938eff09ba722b8828f18c95ef2178556a
ApurvX210 <apurvsharma210@gmail.com>
2026-01-15 17:15:37 +0530

    Identation in peer.go

--- peers/peer.go
+++ peers/peer.go
@@ -10,32 +10,31 @@ type Message struct {
 }
 
 type Peer struct {
-	con net.Conn
+	con     net.Conn
 	msgChan chan Message
 }
 
-func (p *Peer) Send(msg []byte) (int,error){
+func (p *Peer) Send(msg []byte) (int, error) {
 	return p.con.Write(msg)
 }
 
-func NewPeer(connection net.Conn, msgChan chan Message) *Peer{
+func NewPeer(connection net.Conn, msgChan chan Message) *Peer {
 	return &Peer{
-		con : connection,
-		msgChan : msgChan,
+		con:     connection,
+		msgChan: msgChan,
 	}
 }
 
-func (p *Peer) ReadRequest() error{
-	buff := make([]byte,1024)
+func (p *Peer) ReadRequest() error {
+	buff := make([]byte, 1024)
 
 	for {
 		n, err := p.con.Read(buff)
-		if err != nil{
+		if err != nil {
 			return err
 		}
 		msgBuff := make([]byte, n)
-		copy(msgBuff,buff)
-		p.msgChan <- Message{msgBuff,p}
+		copy(msgBuff, buff)
+		p.msgChan <- Message{msgBuff, p}
 	}
-	
 }
