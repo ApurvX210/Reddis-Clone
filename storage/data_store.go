e26345120c3be499f02404b01951285f8547cc72
ApurvX210 <apurvsharma210@gmail.com>
2026-01-20 16:25:24 +0530

    Updated set, get and delete method considering ttl feature

--- storage/data_store.go
+++ storage/data_store.go
@@ -7,23 +7,19 @@ import (
 	"time"
 )
 
-type ExpirationInfo struct{
-	timestamp	time.Time
-}
-
 
 type DB struct {
 	mu   			sync.RWMutex
 	data 			map[string][]byte
 	expiryMu		sync.RWMutex
-	expirationMap	map[string] *ExpirationInfo
+	expirationMap	map[string] time.Time
 	radix			*Radix	
 }
 
 func NewDb() *DB {
 	return &DB{
 		data: map[string][]byte{},
-		expirationMap: make(map[string]*ExpirationInfo),
+		expirationMap: make(map[string]time.Time),
 		radix: &Radix{
 			prefix: "",
 			child: map[string]*Radix{},
@@ -34,26 +30,45 @@ func NewDb() *DB {
 
 func (db *DB) Set(key, val []byte, exp time.Time) error {
 	db.mu.Lock()
-	defer db.mu.Unlock()
+	db.expiryMu.Lock()
+	// Storing data in map
 	db.data[string(key)] = []byte(val)
-	
+	// Storing expiry info in map for future cleanup
+	db.expirationMap[string(key)] = exp
+	db.mu.Unlock()
+	db.expiryMu.Unlock()
 	return nil
 }
 
 func (db *DB) Get(key []byte) ([]byte, bool) {
 	db.mu.RLock()
-	defer db.mu.RUnlock()
-	val, ok := db.data[string(key)]
-	return val, ok
+	db.expiryMu.RLock()
+	Key := string(key)
+	exp,_ := db.expirationMap[Key]
+	var value []byte;
+	var exist bool;
+	if exp.Before(time.Now()){
+		value, exist = db.data[string(key)]
+	}else{
+		value = nil
+		exist = false
+	}
+	db.mu.RUnlock()
+	db.expiryMu.RUnlock()
+	return value, exist
 }
 
 func (db *DB) Del(key []byte) bool {
 	db.mu.Lock()
-	defer db.mu.Unlock()
+	db.expiryMu.Lock()
+
 	_, exists := db.data[string(key)]
 	if exists {
 		delete(db.data, string(key))
+		delete(db.expirationMap, string(key))
 	}
+	db.mu.Unlock()
+	db.expiryMu.Unlock()
 	return exists
 }
 
